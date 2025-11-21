66ea5dfa1b82d8cf7c8c5742de548d53d76486a1
DenisZhmakin <den.zhmakin.00@gmail.com>
2025-11-21 16:55:32 +0300

    feat: Дописал мод.

---
+++ MaxHpResourceEdit/ItemConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using SPTarkov.Server.Core.Models.Common;
+
+namespace MaxHpResourceEdit;
+
+public record ItemConfig
+{
+    [JsonPropertyName("mongo_id")]
+    public required MongoId MongoId { get; init; }
+    
+    [JsonPropertyName("value")]
+    public required int Value { get; init; }
+};
---
+++ MaxHpResourceEdit/MaxHpResourceEditExtension.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Logging;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Servers;
+
+namespace MaxHpResourceEdit;
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class MaxHpResourceEditExtension(
+    ModHelper modHelper, 
+    DatabaseServer databaseServer,
+    ISptLogger<MaxHpResourceEditExtension> logger) : IOnLoad
+{
+    public Task OnLoad()
+    {
+        var tables = databaseServer.GetTables();
+        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        var items = modHelper.GetJsonDataFromFile<ItemConfig[]>(pathToMod, "config.json");
+        
+        logger.LogWithColor("[MaxHpResourceEdit] The mod is loaded.", LogTextColor.Green);
+
+        foreach (var item in items)
+        {
+            if (tables.Templates.Items[item.MongoId].Properties is null)
+            {
+                logger.Error($"[MaxHpResourceEdit] Item {item.MongoId} has no properties");
+                continue;
+            }
+            
+            tables.Templates.Items[item.MongoId].Properties!.MaxHpResource = item.Value;
+        }
+        
+        return Task.CompletedTask;
+    }
+}
